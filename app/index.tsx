c01255fa38c76ff1cf932108948143593fa4088f
Edson Carreon <edsonjaybcarreon@gmail.com>
2024-07-20 00:08:56 +0800

    added tailwind support

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,9 @@
 import { Text, View } from "react-native";
+import { tw } from "../tailwind";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        alignItems: "center",
-        flex: 1,
-        justifyContent: "center",
-      }}
-    >
+    <View style={tw`flex-1 items-center justify-center`}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
