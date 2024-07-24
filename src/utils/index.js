1eb761edaf94e9cc8d02b46c136746a48f1b6cb7
Edson Carreon <edsonjaybcarreon@gmail.com>
2024-07-24 19:43:35 +0800

    chore: Update .env.template and add scripts for mobile app creation

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
 import { execSync } from 'child_process';
 import fs from 'fs';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
 
 export function getDirName() {
-  return __dirname;
+  const __filename = fileURLToPath(import.meta.url);
+  return dirname(__filename);
 }
 
 export const executeCommand = (dir, command) => {
@@ -11,7 +13,7 @@ export const executeCommand = (dir, command) => {
 };
 
 export const copyFile = (destinationPath, fileName) => {
-  const sourcePath = path.join(getDirName(), `../templates/${fileName}`);
+  const sourcePath = path.join(getDirName(), `../../templates/${fileName}`);
 
   fs.copyFile(sourcePath, destinationPath, (err) => {
     if (err) {
@@ -22,7 +24,7 @@ export const copyFile = (destinationPath, fileName) => {
 };
 
 export const copyDir = (destinationPath, fileName) => {
-  const sourcePath = path.join(getDirName(), `../templates/${fileName}`);
+  const sourcePath = path.join(getDirName(), `../../templates/${fileName}`);
 
   fs.cpSync(sourcePath, destinationPath, { recursive: true }, (err) => {
     if (err) {
@@ -43,22 +45,11 @@ export const removeDir = (dir) => {
 
 export const addScriptsInRootPackageJson = (dir) => {
   const scripts = {
-    'android:connect': 'cd apps/mobile && npm run android:connect',
-    'check-env:mobile': './check-env.sh apps/mobile/.env apps/mobile/.env.template',
+    'android:connect': 'adb reverse tcp:8081 tcp:8081',
+    'list:ios-devices': 'xcrun xctrace list devices',
+    'check-env:mobile': `./check-env.sh ${dir}/.env ${dir}/.env.template'`,
     clean: './clean-generated-outputs.sh',
-    'create-env': 'printenv > ',
-    'lint:all': 'npx nx run-many -t lint -p mobile --parallel=1 --skip-nx-cache',
-    'lint:mobile': 'npx nx run mobile:lint --skip-nx-cache',
-    'mobile-android': 'cd apps/mobile && npm run run-android',
-    'mobile-ios': 'cd apps/mobile && npm run run-ios',
     prepare: 'husky install',
-    'serve:mobile': 'cd apps/mobile && npm start',
-    'serve:all': 'npx nx run-many -t serve -p mobile --parallel=1 --skip-nx-cache',
-    xcode: 'cd apps/mobile && npm run xcode',
-    'setup-fastlane': 'cd apps/mobile && npm run setup-fastlane',
-    'deploy-android:dev': 'cd apps/mobile && npm run deploy-android:dev',
-    'deploy-ios:dev': 'cd apps/mobile && npm run deploy-ios:dev',
-    'ios-certificates': 'cd apps/mobile && npm run ios-certificates',
   };
 
   const packageJsonPath = path.join(dir, 'package.json');
